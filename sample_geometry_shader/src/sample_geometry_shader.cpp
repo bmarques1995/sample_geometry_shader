e8434150623383696c73557353fb3210466c9dac
bmarques1995 <testebash@outlook.com>
2025-02-04 22:52:02 -0300

    Startup C++ project

---
+++ sample_geometry_shader/src/sample_geometry_shader.cpp
@@ -0,0 +1,12 @@
+﻿// sample_geometry_shader.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "sample_geometry_shader.hpp"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
